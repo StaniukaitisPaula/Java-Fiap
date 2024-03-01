70546764005c2f867c593fad7f644d9e45ac326b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-01 09:01:46 -0300

    parte o1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
 
+        Scanner leitor = new Scanner(System.in);
+
         System.out.println("Bem vindos!!");
+        //Tipo nomeVariavél
+        String nomeAluno, telefone; // String -> tipo class
+        int idade;                 // int - tipo primitivo
+
+        System.out.println("Digite seu nome:");
+        nomeAluno = leitor.nextLine();
+        System.out.println("Seja bem vindo a Fiap " + nomeAluno);
+
+
+
+
 
     }
 }
